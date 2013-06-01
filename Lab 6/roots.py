fce1eccbcb1c2fbae12c66548e97482d094923a7
Chris Dudiak <cdudiak@me.com>
2013-06-01 02:56:25 -0700

    Began work on the code portion after finishing the derivation.

---
+++ Lab 6/roots.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Chris Dudiak
+# This program contains various methods to solve for the roots of functions.
+
+import math
+import numpy as np
+import operator as op
+import sys
+import matplotlib.pyplot as plt
+
+
+def roots():
+    
+    print "Successfully Ran Script"
+    
+    
+        
+    
+if __name__ == "__main__":
+    print "Running Methods on default function: f(x) = sin(x) - .45"
+    roots()
